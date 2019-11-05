5ac7d077c9c9825dcc0d4928eca9de8e57d9e6be
Hank Hester <wihest@microsoft.com>
2019-11-05 14:54:08 -0800

    Copy code from hellonextjs

---
+++ config.ts
@@ -0,0 +1,8 @@
+const config = {
+    endpoint: process.env.COSMOS_ENDPOINT,
+    key: process.env.COSMOS_KEY,
+    database: process.env.COSMOS_DATABASE,
+    container: process.env.COSMOS_CONTAINER
+};
+
+export default config;
