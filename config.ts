5bf70360649570481d797893b212702bac04a245
Hank Hester <wihest@microsoft.com>
2019-11-06 15:57:34 -0800

    Delete config.ts

--- config.ts
+++
@@ -1,8 +0,0 @@
-const config = {
-    endpoint: process.env.COSMOS_ENDPOINT,
-    key: process.env.COSMOS_KEY,
-    database: process.env.COSMOS_DATABASE,
-    container: process.env.COSMOS_CONTAINER
-};
-
-export default config;
