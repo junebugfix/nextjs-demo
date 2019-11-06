4aae371635bae8cc224d8431e524b404d5cc2f67
Hank Hester <wihest@microsoft.com>
2019-11-06 10:58:59 -0800

    Merge index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -31,8 +31,4 @@ Home.getInitialProps = async function() {
     return { name: 'Unknown'};
 }
 
-<<<<<<< HEAD
 export default Home;
-=======
-export default Home;
->>>>>>> 433978fcf8ebc3d0cc6dbe97a6d498448dd7d9b9
