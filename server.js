3fe6422db5df43275a78086922c53ae8133e6712
Hank Hester <wihest@microsoft.com>
2019-11-06 11:16:37 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,32 +1,30 @@
 const express = require("express");
-// const next = require("next");
+const next = require("next");
 
 const dev = process.env.NODE_ENV !== "production";
-// const app = next({ dev });
-// const handle = app.getRequestHandler();
+const app = next({ dev });
+const handle = app.getRequestHandler();
 const port = process.env.PORT || 3000;
 
-// app
-//   .prepare()
-//   .then(() => {
-//     const server = express();
+app
+  .prepare()
+  .then(() => {
+    const server = express();
 
-//     server.get("*", (req, res) => {
-//       return handle(req, res);
-//     });
+    server.get("*", (req, res) => {
+      return handle(req, res);
+    });
 
-//     server.listen(port, err => {
-//       if (err) throw err;
-//       console.log(`Ready on port ${port}`);
-//     });
-//   })
-//   .catch(ex => {
-//     console.error(ex.stack);
-//     process.exit(1);
-//   });
+    server.listen(port, err => {
+      if (err) throw err;
+      console.log(`Ready on port ${port}`);
+    });
+  })
+  .catch(ex => {
+    console.error(ex.stack);
+    process.exit(1);
+  });
 
-const app = express()
-
-app.get('/', (req, res) => res.send('Hello World!'))
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+// const app = express() 
+// app.get('/', (req, res) => res.send('Hello World!'))
+// app.listen(port, () => console.log(`Example app listening on port ${port}!`))
